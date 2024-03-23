61729a356275dbd9752e704b3f6708f951270bb5
Nostalia17 <155804556+Nostalia17@users.noreply.github.com>
2024-03-23 12:01:43 +0800

    Update script.js

--- features-spellbook/Assets/JS/script.js
+++ features-spellbook/Assets/JS/script.js
@@ -3,6 +3,6 @@ document.getElementById('launchButton').addEventListener('click', function(event
     document.body.classList.add('fadeout'); // Add the 'fadeout' class to the body
 
     setTimeout(function() {
-        window.location.href = 'newPage.html'; // Redirect to newPage.html after 6 seconds
+        window.location.href = "https://precious-carbonell.github.io/Hogwarts/features-spellbook/newpage.html"; // Redirect to newPage.html after 6 seconds
     }, 6000); // Change 3000 to 6000
 });
