185a235dc95c304a01debdcf9ad974ffb40748a2
Nostalia17 <155804556+Nostalia17@users.noreply.github.com>
2024-03-23 11:37:38 +0800

    Update script.js

--- features-spellbook/Assets/JS/script.js
+++ features-spellbook/Assets/JS/script.js
@@ -6,3 +6,10 @@ document.getElementById('launchButton').addEventListener('click', function(event
         window.location.href = 'newPage.html'; // Redirect to newPage.html after 6 seconds
     }, 6000); // Change 3000 to 6000
 });
+
+
+document.getElementById('exitButton').addEventListener('click', function() {
+    window.location.href = '.../index.html'; // Change 'previousPage.html' to the appropriate page
+});
+
+    <button id="exitButton">&#8592;</button>
